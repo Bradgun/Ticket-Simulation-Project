cbc6354a0d169de94e08d1e4f608b567ac3dc2b6
Bradgun <bradleydgunawan@gmail.com>
2024-05-08 20:30:31 -0700

    update

---
+++ CLASS.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public static void main(String args[])
+{
+    Scanner scnr = new Scanner(System.in);
+}
+
+public boolean checkValidity(int month, int day)
+{
+
+}
+
+public double calculateTicketPrice(int month, int day)
+{
+    if (month = 6 || month = 7 || month = 8)
+}
